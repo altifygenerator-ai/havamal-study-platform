710c72ee34931008b26bc019a6ce9c57c8da13f8
J H <altifygenerator@gmail.com>
2026-07-22 19:07:35 -0500

    robots

--- app/robots.ts
+++ app/robots.ts
@@ -1,3 +1,21 @@
 import type { MetadataRoute } from "next";
 import { siteConfig } from "@/lib/site";
-export default function robots():MetadataRoute.Robots{return{rules:{userAgent:"*",allow:"/",disallow:["/admin/","/account/","/my-study/","/api/"]},sitemap:`${siteConfig.url}/sitemap.xml`}}
+
+export default function robots(): MetadataRoute.Robots {
+  const baseUrl = siteConfig.url.replace(/\/$/, "");
+
+  return {
+    rules: {
+      userAgent: "*",
+      allow: "/",
+      disallow: [
+        "/admin",
+        "/account",
+        "/my-study",
+        "/api",
+      ],
+    },
+    sitemap: `${baseUrl}/sitemap.xml`,
+    host: baseUrl,
+  };
+}
